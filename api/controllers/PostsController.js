4f60679da1cb8c86c88f670f123ad66e540eec77
numi3257 <numi3257@gmail.com>
2020-03-19 23:16:48 +0100

    Create first simple post

--- api/controllers/PostsController.js
+++ api/controllers/PostsController.js
@@ -1,5 +1,53 @@
+// dummy database
+const post1 = {
+    id: 1,
+    title: 'POST TITLE 1',
+    body: 'This is my body'
+}
+const post2 = {
+    id: 2,
+    title: 'POST TITLE 2',
+    body: 'This is my body2'
+}
+const post3 = {
+    id: 3,
+    title: 'POST TITLE 3',
+    body: 'This is my body3'
+}
+
+const allPosts = [post1, post2, post3]
+
+
 module.exports = {
     posts: function (req, res) {
-        res.send('Here I\'m hitting the /posts endpoint322')
+        res.send(allPosts)
+    },
+
+
+    create: function (req, res) {
+        const title = req.param('title')
+        const body = req.param('body')
+
+        console.log(title + " " + body)
+        sails.log.debug(title + " " + body)
+        sails.log.warn(title + " " + body)
+
+
+        const newPost = { id: 4, title: title, body: body }
+        allPosts.push(newPost)
+
+        res.end()
+
+    },
+
+    findById: function (req, res) {
+        const postId = req.param('postId')
+
+        const filteredPosts = allPosts.filter(p => { return p.id == postId })
+        if (filteredPosts.length > 0) {
+            res.send(filteredPosts[0])
+        } else {
+            res.send('Feiled to find post by id: ' + postId)
+        }
     }
 }
--- config/routes.js
+++ config/routes.js
@@ -21,7 +21,9 @@ module.exports.routes = {
 
   '/': { view: 'pages/homepage' },
 
-  '/posts': 'PostsController.posts'
+  '/posts': 'PostsController.posts',
+  '/findById/:postId': 'PostsController.findById',
+  '/create': 'PostsController.create'
 
   /***************************************************************************
   *                                                                          *
